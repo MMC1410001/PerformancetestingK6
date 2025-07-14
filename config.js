f528c8b043a2d2ffee12bd6572746797cb873839
MMC1410001 <92584193+MMC1410001@users.noreply.github.com>
2025-07-14 19:49:13 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  stopOnFailure: true,   // Set to false if we need to continue running the files even after failure is observed
-  testFolder: './tests',
-  outputFolder: './reports',
-  generateCharts: true // ⬅️ Enable chart generation
-};
