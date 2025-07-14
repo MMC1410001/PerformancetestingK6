c9292cceebfd3df4c6746618428ec8f011018e58
MMC1410001 <92584193+MMC1410001@users.noreply.github.com>
2025-07-14 19:49:02 +0530

    Delete check-env.js

--- check-env.js
+++
@@ -1,4 +0,0 @@
-// To check if .env variables are being accessed from different file locations of the project
-require('dotenv').config();
-console.log('ALERT_EMAIL_USER:', process.env.ALERT_EMAIL_USER);
-console.log('ALERT_EMAIL_PASS is set:', !!process.env.ALERT_EMAIL_PASS);
